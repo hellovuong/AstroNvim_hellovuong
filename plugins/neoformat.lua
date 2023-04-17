dc64cd087d8ecf0c97c2edd0c83db0dabcc79b34
Long Vuong <vuong@wheel.me>
2023-04-17 18:49:28 +0200

    Update Neoformat

--- plugins/neoformat.lua
+++ plugins/neoformat.lua
@@ -4,7 +4,7 @@ return {
     event = "LspAttach",
     init = function()
      vim.g.neoformat_enabled_cpp = "clangformat"
-     vim.g.neoformat_cpp_clangformat = {exe = 'clang-format', args = { ["--style"] = file} }
+     vim.g.neoformat_cpp_clangformat = {exe = 'clang-format', args = { ["--style"] = 'file'} }
     end,
    }
 }
