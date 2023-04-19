6f33688fde501dae9d20ca5786829f9d446350da
Long Vuong <vuong@wheel.me>
2023-04-19 12:53:28 +0200

    nightfox theme

--- init.lua
+++ init.lua
@@ -18,7 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- colorscheme = "astrodark",
+  colorscheme = "nightfox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
